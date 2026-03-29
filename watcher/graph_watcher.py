9769fc8cfa2075cf4c0162230b559ea6516e38bf
John Hawver <johnhawver2@gmail.com>
2026-03-29 15:26:21 -0400

    feat(watcher): parse pod events into graph updates - Convert K8s pod API objects to PodNode via pod_to_node() - Parse CPU millicores and memory bytes from container resource specs - Stream pod ADDED/MODIFIED/DELETED events via watch.Watch() - Skip kube-system pods; auto-retry with 2s backoff on disconnect

---
+++ watcher/graph_watcher.py
@@ -0,0 +1,44 @@
+import logging
+import time
+from kubernetes import client, config
+
+from .graph_model import PodDependencyGraph
+from .pod_watcher import PodWatcher
+from .service_watcher import ServiceWatcher
+from .pvc_watcher import PVCWatcher
+
+log = logging.getLogger(__name__)
+
+
+class GraphWatcher:
+    def __init__(self):
+        self.graph = PodDependencyGraph()
+        config.load_kube_config()
+        self.v1 = client.CoreV1Api()
+        self.pod_watcher = PodWatcher(self.graph)
+        self.service_watcher = ServiceWatcher(self.graph, self.v1)
+        self.pvc_watcher = PVCWatcher(self.graph, self.v1)
+
+    def start(self):
+        self.pod_watcher.start()
+        # Give pod watcher 2 seconds to populate initial pods
+        # before service/pvc watchers try to look them up
+        time.sleep(2)
+        self.service_watcher.start()
+        self.pvc_watcher.start()
+        log.info("GraphWatcher fully started")
+
+    def get_graph(self):
+        return self.graph.get_snapshot()
+
+    def stats(self):
+        return self.graph.stats()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(message)s')
+    watcher = GraphWatcher()
+    watcher.start()
+    while True:
+        log.info(f"Graph stats: {watcher.stats()}")
+        time.sleep(5)
